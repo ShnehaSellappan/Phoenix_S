9c27e17f042776833a26fc5d305e37011eeca415
ShnehaSellappan <41229922+ShnehaSellappan@users.noreply.github.com>
2018-07-14 14:25:57 +0530

    Create posneg.java

---
+++ posneg./posneg.java
@@ -0,0 +1,19 @@
+public class Posneg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n;
+Scanner pn=new Scanner(System.in);
+n =pn.nextInt();
+if(n>0){
+	System.out.println("Positive");
+}
+else if(n<0) {
+	System.out.println("Negative");
+}
+else {
+	System.out.println("Zero");
+}
+	}
+
+}
