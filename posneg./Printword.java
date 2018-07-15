17f1a9fe1bcf6b684bd5e278ebdc916135f1288f
ShnehaSellappan <41229922+ShnehaSellappan@users.noreply.github.com>
2018-07-15 14:46:41 +0530

    Create Printword.java

---
+++ posneg./Printword.java
@@ -0,0 +1,14 @@
+public class Printntime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n,a=0;
+System.out.println("Enter n value:");
+Scanner wp=new Scanner(System.in);
+n =wp.nextInt();
+for(int i=0;i<n;i++) {
+	System.out.println("Hello");
+}
+	}
+
+}
