62f163a5b9c081bdb345ee1093f34f69e421116b
ShnehaSellappan <41229922+ShnehaSellappan@users.noreply.github.com>
2018-07-17 17:46:24 +0530

    Create Sum.java

---
+++ posneg./Sum.java
@@ -0,0 +1,20 @@
+public class Sumofkint {
+
+	public static void main(String[] args) {
+  int n,k,i,sum=0;
+  System.out.println("n:");
+  System.out.println("Enter k:");
+  System.out.println("Elements:");
+  Scanner nk=new Scanner(System.in);
+  n =nk.nextInt();
+  k =nk.nextInt();
+  int a[]=new int[n];
+for(i=0;i<a.length;i++) {
+	a[i] =nk.nextInt();
+}
+for(i=0;i<k;i++) {
+	sum=sum+a[i];
+}
+System.out.println(sum);
+	}
+}
