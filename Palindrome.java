049c514bea83f931d274773d77bf02643d09d10e
ShnehaSellappan <41229922+ShnehaSellappan@users.noreply.github.com>
2018-07-20 14:25:11 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -6,16 +6,18 @@ int n,r,q = 0,k=0,s,l;
 Scanner pl=new Scanner(System.in);
 n =pl.nextInt();
 s=n;
+		if(n<=1000)
 while(n!=0) {
 	r=n%10;
     n=n/10;
    q=q*10+r;
 }
 if(s==q) {
-System.out.println("R");
+System.out.println("Yes");
 	}
 else {
-	System.out.println("p");
+	System.out.println("No");
 }
+	}
 	}
 }
