a1fc82be31274468050aa3e66fb4ec58778a088e
ShnehaSellappan <41229922+ShnehaSellappan@users.noreply.github.com>
2018-07-23 09:30:34 +0530

    Create Multiples.java

---
+++ Multiples.java
@@ -0,0 +1,14 @@
+public class Multiples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,N,mul;
+Scanner mu=new Scanner(System.in);
+N =mu.nextInt();
+for(i=1;i<=5;i++) {
+	mul=N*i;
+	System.out.println(mul);
+}
+	}
+
+}
