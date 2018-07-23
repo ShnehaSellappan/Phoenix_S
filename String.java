6d7b8dea3d244fe840bb9ca1c0ef94781024f0ec
ShnehaSellappan <41229922+ShnehaSellappan@users.noreply.github.com>
2018-07-23 15:46:34 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,11 @@
+public class Reverse {
+
+	public static void main(String[] args) {
+String str;
+Scanner rs=new Scanner(System.in);
+str =rs.next();
+String str1=new StringBuffer(str).reverse().toString();
+System.out.println(str1);
+	}
+
+}
