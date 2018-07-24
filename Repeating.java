a00fb24de562fa88b019460fd1ac5545c338eb3a
ShnehaSellappan <41229922+ShnehaSellappan@users.noreply.github.com>
2018-07-24 17:36:06 +0530

    Create Repeating.java

---
+++ Repeating.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Duplieleminate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int N;
+Scanner dn=new Scanner(System.in);
+N =dn.nextInt();
+int i,j,count=0;
+	int a[]=new int[N];
+	for(i=0;i<a.length;i++) {
+		a[i]=dn.nextInt();
+	}
+ for(i=0;i<a.length;i++) {
+	   count=0;
+	 for(j=i;j<a.length;j++) {
+		 if(a[i]==a[j]) {
+			 count++;
+		 }
+	 }
+ if(count==2) {
+	 System.out.println(a[i]+" ");
+ }
+ }
+}
+	}
+
