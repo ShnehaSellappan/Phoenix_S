877985b122ab643bcdb8e3216097285d6ca972ae
ShnehaSellappan <41229922+ShnehaSellappan@users.noreply.github.com>
2018-07-24 18:35:27 +0530

    Update Repeating.java

--- Repeating.java
+++ Repeating.java
@@ -6,7 +6,7 @@ public class Duplieleminate {
 int N;
 Scanner dn=new Scanner(System.in);
 N =dn.nextInt();
-int i,j,count=0;
+int i,j,count=0,count1=0;
 if(N<=100000) {
 	int a[]=new int[N];
 	for(i=0;i<a.length;i++) {
@@ -14,14 +14,18 @@ if(N<=100000) {
 	}
  for(i=0;i<a.length;i++) {
 	   count=0;
-	 for(j=i;j<a.length;j++) {
+	 for(j=i+1;j<a.length;j++) {
 		 if(a[i]==a[j]) {
 			 count++;
 		 }
 	 }
- if(count==2) {
-	 System.out.println(a[i]+" ");
+ if(count==1) {
+	 System.out.print(a[i]+" ");
+	  count1++;
  }
+
+ } if(count1==0){
+	 System.out.println("Unique");
  }
 }
 	}
