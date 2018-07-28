2b9d6c62324b67c74bb5aef34025493b72a93dbe
ShnehaSellappan <41229922+ShnehaSellappan@users.noreply.github.com>
2018-07-28 09:31:02 +0530

    Create Temperature.java

---
+++ Temperature.java
@@ -0,0 +1,15 @@
+package Github;
+import java.util.Scanner;
+public class Temoerature {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int c,k;
+System.out.println("Enter celsius:");
+Scanner kc=new Scanner(System.in);
+c=kc.nextInt();
+k=273+c;
+System.out.println(k);
+	}
+
+}
